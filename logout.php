503dc2ba73e12449840c43a78af21ba9654bd553
Niccolo <niccolochi09@gmail.com>
2025-11-20 15:32:46 +0800

    updated logout.php to cleas OTP properly and prevents accidental session reuse on some browsers

--- logout.php
+++ logout.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
+
+// Explicitly remove all sensitive 2FA session data
+unset($_SESSION['otp_code']);
+unset($_SESSION['otp_expires']);
+unset($_SESSION['2fa_passed']);
+
+// Remove all session data
 session_unset();
+
+// Destroy the session fully
 session_destroy();
+
+// Redirect to login page
 header("Location: LoginPage/login.php");
 exit;
 ?>
